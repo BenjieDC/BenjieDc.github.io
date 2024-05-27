ca8cded9f5228b3882388362f5a7462faa1fc9ab
BenjieDC <delacruzbenjie84@gmail.com>
2024-05-27 14:43:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function countLike1() {
 btnLike1.addEventListener("click", countLike1)
 
 const btnLike2 = document.getElementById("btnLike2")
-const count1 = document.getElementById("count1")
+const count2 = document.getElementById("count2")
 
 function countLike2() {
-  let likeCounts = parseInt(count1.value) + 1
+  let likeCounts = parseInt(count2.value) + 1
   count1.textContent = likeCounts.toString()
 }
 
-btnLike2.addEventListener("click", countLike1)
+btnLike2.addEventListener("click", countLike2)
