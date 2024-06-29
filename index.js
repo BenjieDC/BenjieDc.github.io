1c9675375b1c6bf332d822b955301f6137b4aa53
BenjieDC <delacruzbenjie84@gmail.com>
2024-06-29 15:44:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+var product1 = document.getElementById("product1")
+var qty1 = document.getElementById("qty1")
+var price1 = document.getElementById("price1")
+
+var product2 document.getElementById("product2")
+var qty2 = document.getElementById("qty2")
+var price2 = document.getElementById("price2")
+
+var carts = document.getElementById("carts")
+var total = document.getElementById("total")
+var cash = document.getElementById("cash")
+var change = document.getElementById("change")  
+
+function addOrder () {  
+    carts.textcontent=""
+    if (parsefloat (qty1.value) > 0) {
+    var order= qtyl.value.toString() + 'pc/s x '+ pricel.textContent + textContent += carts.value.toString() + "\n"; +product1.textContent Php' (parseFloat(qtyl.value) *parseFloat(pricel.textContent)) + '\n'
+    xtContent += order
+    
+    at(qty2.value) > 0) {
+    r= qty2.value.toString() + pc/s x + price2.textContent ++ product2.textContent Php (parseFloat(qty2.value) *parseFloat (price2.textContent)) \n'
+      
+    textContent += carts.value.toString() + "\n";
+    
+    xtContent += order
+
+tener("keyup", addOrder):
+tener("keyup", addOrder
+
 /*const btnLike1 = document.getElementById("btnLike1")
 const count1 = document.getElementById("count1")
 
